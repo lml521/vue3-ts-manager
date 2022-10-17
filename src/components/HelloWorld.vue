4388fa0ecac0e9f818b97e608b05e7ea26fa989a
lml <2084387897@qq.com>
2022-10-17 11:13:02 +0800

    初始化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
@@ -8,31 +8,28 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
 }
-</style>
+</style> -->
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { useStore } from "../store";
+const store = useStore();
+const count = ref(0);
+const showcount = computed(() => {
+  return store.getters["getCount"];
+});
+const addBtn = () => {
+  store.commit("setCount", ++count.value);
+};
+</script>
+<template>
+  <p>{{ showcount }}</p>
+  <button @click="addBtn">增加</button>
+</template>
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  // 解决 Network: use --host to expose
+  server: {
+    host: '0.0.0.0', //解决“vite use `--host` to expose”
+    port: 8080,
+    open: true
+  },
+  // 解决 @ 
+  resolve: {
+    alias: [{
+      find: '@',
+      replacement:resolve(__dirname,'src')
+   }]
+ }
 })
